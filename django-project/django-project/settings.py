edf88f95f263f2f31c81246e60af6813f4e88322
c16matan <37793866+c16matan@users.noreply.github.com>
2019-03-16 21:03:31 +0100

    Installed search app and added shared template folder

--- django-project/django-project/settings.py
+++ django-project/django-project/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'search',
 ]
 
 MIDDLEWARE = [
@@ -42,7 +43,9 @@ ROOT_URLCONF = 'django-project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            (os.path.join(BASE_DIR, 'templates')),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
