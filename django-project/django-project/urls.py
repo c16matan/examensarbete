288bb9a499f4a1b845f123c66028cb146145e7f9
c16matan <37793866+c16matan@users.noreply.github.com>
2019-03-18 22:05:20 +0100

    Renamed question app to questions

--- django-project/django-project/urls.py
+++ django-project/django-project/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('question.urls')),
+    path('', include('questions.urls')),
 ]
--- django-project/questions/views.py
+++ django-project/questions/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'question/index.html')
+    return render(request, 'questions/index.html')
