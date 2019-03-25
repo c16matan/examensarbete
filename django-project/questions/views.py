ba302ae2906d269349e53021903ea744c948fdf5
c16matan <37793866+c16matan@users.noreply.github.com>
2019-03-25 16:56:48 +0100

    Index view using "questions" models
    The index view for the questions app is now using the Post model to get the most recent questions. The template file has been updated to use the new variables. The text body will always be inline.

--- django-project/questions/views.py
+++ django-project/questions/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
 
 
 def index(request):
-    title = 'What the actual?'
-    body = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dapibus, ipsum id consectetur porttitor, augue lorem vehicula justo, quis vestibulum sapien est vel dolor. Quisque convallis eu neque eget molestie. Praesent faucibus ipsum quam, eu sollicitudin elit molestie sed. Duis nec dolor purus. Duis sit amet mauris feugiat, facilisis ex eu, pellentesque dui. Curabitur pharetra felis massa, quis hendrerit tortor sagittis ac. Suspendisse nulla ipsum, mattis a vehicula vel, porttitor sed purus. Integer lobortis imperdiet lobortis. Quisque tempus, diam non eleifend luctus, dui velit auctor elit, sed tincidunt leo nisl et justo.'
-    date = '8th of September'
-    data = []
-    for i in range(15):
-        data.append({
-            'votes': 40,
-            'answers': 4,
-            'title': title,
-            'body': body,
-            'date': date
-        })
+    # Posts with post_type=1 is the questions
+    posts = Post.objects \
+        .filter(post_type=1) \
+        .order_by('-id'). \
+        all()[:30].values()
 
     return render(request, 'questions/index.html', {
-        'questions': data
+        'questions': posts
     })
