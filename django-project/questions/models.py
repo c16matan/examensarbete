1b665ae5db135b6eba7487b15ace456d38a2d003
c16matan <37793866+c16matan@users.noreply.github.com>
2019-03-25 13:06:11 +0100

    Added "questions" app models
    The models does not follow the database.sql schema and the database file might be removed in the future as it might not be needed. These models is more accurate when it comes to null values and whatnot when comparing it to the Stack Overflow dataset. The models has been registered in admin.py so that they are available in the admin panel (/admin).

--- django-project/questions/models.py
+++ django-project/questions/models.py
@@ -1,3 +1,25 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    post_type = models.SmallIntegerField(default=1)
+    parent_id = models.BigIntegerField(null=True)
+    accepted_answer = models.BigIntegerField(null=True)
+    score = models.SmallIntegerField()
+    view_count = models.IntegerField(null=True)
+    answer_count = models.SmallIntegerField(null=True)
+    title = models.CharField(max_length=150, null=True)
+    body = models.TextField()
+    comment_count = models.SmallIntegerField()
+    creation_date = models.DateTimeField(default=datetime.now)
+    last_edit_date = models.DateTimeField(null=True)
+    last_activity_date = models.DateTimeField(null=True)
+
+
+class Comment(models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.TextField(null=True)
+    creation_date = models.DateTimeField(default=datetime.now)
