7b6d88d9eca12f4ac7a53f931528a2da974b8737
c16matan <37793866+c16matan@users.noreply.github.com>
2019-04-01 01:52:56 +0200

    Added question route and template
    Added the question route and template for the node.js project.
    The post-preview template is now using an absolute link for the question
    url.

--- nodejs-project/routes/questions.js
+++ nodejs-project/routes/questions.js
@@ -18,4 +18,23 @@ router.get('/search/:search', function (req, res, next) {
     });
 });
 
+router.get('/question/:id', function (req, res, next) {
+    res.render('question', {
+        posts: [{
+            id: req.params.id,
+            creation_date: "2019",
+            last_edit_date: '2020',
+            answer_count: "10",
+            score: 5,
+            body: '<p>test</p> ',
+            title: 'NeoVim'
+        }],
+        comments: [{
+            post_id: req.params.id,
+            text: '<p>Test comment</p>',
+            creation_date: '2019'
+        }]
+    });
+});
+
 module.exports = router;
