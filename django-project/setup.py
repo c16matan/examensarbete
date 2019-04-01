bc71750bd408092c4efd203f102a3997a250c938
c16matan <37793866+c16matan@users.noreply.github.com>
2019-04-01 15:45:28 +0200

    Updated to Django 2.2
    Django 2.2 was released today and will have support for at least 3
    years.

--- django-project/setup.py
+++ django-project/setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'django-project',
-    version = '0.1',
-    url = 'https://github.com/c16matan/examensarbete',
-    packages = find_packages(),
-    install_requires = [
-        'django == 2.1.7',
+    name='django-project',
+    version='0.1',
+    url='https://github.com/c16matan/examensarbete',
+    packages=find_packages(),
+    install_requires=[
+        'django == 2.2',
         'psycopg2 == 2.7.7',
     ],
 )
